b682d5d0d410a0ce6fc2611c0d1a3c0c88ce52c6
NoahOnFyre <noahonfyre@gmail.com>
2023-02-10 23:36:51 +0100

    Updated launcher design and combined JS.

--- main.js
+++ main.js
@@ -1,31 +1,32 @@
-const { app, BrowserWindow, ipcMain } = require('electron')
-const { spawn } = require('child_process')
-const { os } = require('os');
+const { app, BrowserWindow, ipcMain, Notification, Tray, Menu, nativeImage, globalShortcut } = require('electron')
 
-const createWindow = () => {
+const createMainWindow = () => {
     const window = new BrowserWindow({
         width: 1200,
-        height: 679
-        // For a HTML file height of 650 (+29)
+        height: 650,
+        fullscreenable: false,
+        frame: false,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
     })
 
     window.loadFile("index.html")
     window.setMenuBarVisibility(false)
     window.setResizable(false)
     window.setIcon("src/favicon.png")
-
-    ipcMain.on('button-clicked', launchGame)
+    window.setMovable(false)
 }
 
 app.whenReady().then(() => {
-    createWindow()
-    openFolder("%userprofile%\\AppData\\Roaming\\.minecraft\\mods")
+    createMainWindow()
+    app.setAppUserModelId("NeonCore Launcher");
+    globalShortcut.register("Ctrl+Space", () => {
+        console.log("Test")
+    })
 })
 
 app.on("window-all-closed", () => {
     if (process.platform !== "darwin") app.quit()
 })
-
-function openFolder(path) {
-    const explorer = spawn('explorer.exe', [path]);
-}
--- script.js
+++ script.js
@@ -1,5 +1,58 @@
-const launchButton = document.getElementById('launchButton')
+const { app, BrowserWindow, ipcMain, Notification, Tray, Menu, nativeImage, globalShortcut } = require('electron')
+const icon = nativeImage.createFromPath("src/favicon.png")
+const os = require('os')
+const fs = require('fs')
+const child_process = require('child_process')
 
-launchButton.addEventListener('click', function() {
-    launchButton.innerHTML = "Launching..."
+const exitButton = document.getElementById("exitButton")
+const modButton = document.getElementById("modButton")
+const versionSelector = document.getElementById("versionSelector")
+const loaderSection = document.getElementById("loader")
+const mainSection = document.getElementById("main")
+
+exitButton.addEventListener("click", exit)
+
+function exit() {
+    close()
+}
+
+function sleep(seconds) {
+    return new Promise(resolve => setTimeout(resolve, seconds*1000));
+}
+
+function checkAppDataFolder() {
+    const userInfo = os.userInfo()
+    let appDataDir = userInfo.homedir + "\\AppData\\Roaming\\NeonCore"
+    if(!fs.existsSync(appDataDir)) {
+        fs.mkdirSync(appDataDir, { recursive: true })
+        console.log("AppData folder was created!")
+    }
+}
+
+function openModsFolder(version) {
+    const userInfo = os.userInfo();
+    let modDir = userInfo.homedir + "\\AppData\\Roaming\\NeonCore\\mods\\" + version + ""
+    if(!fs.existsSync(modDir)) {
+        fs.mkdirSync(modDir, { recursive: true })
+        console.log("Mods folder was created!")
+    }
+    child_process.exec("start explorer.exe " + "\"" + modDir + "\"")
+}
+
+function showNotification(notificationTitle, notificationBody) {
+    const notification = new Notification({ title: notificationTitle, body: notificationBody, icon: icon, silent: false}).show()
+}
+
+async function load() {
+    loaderSection.style.display = "grid"
+    mainSection.style.display = "none"
+    await sleep(2)
+    loaderSection.style.display = "none"
+    mainSection.style.display = "block"
+}
+
+load()
+
+modButton.addEventListener("click", () => {
+    openModsFolder(versionSelector.value)
 })
